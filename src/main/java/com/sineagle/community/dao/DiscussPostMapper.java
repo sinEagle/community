186ab3aae6e9bf9a1cddc7ff9e3167cd3838b45a
sineagle <997991067@qq.com>
2022-11-06 15:59:24 +0800

    completed index community

---
+++ src/main/java/com/sineagle/community/dao/DiscussPostMapper.java
@@ -0,0 +1,16 @@
+package com.sineagle.community.dao;
+
+import com.sineagle.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+}
