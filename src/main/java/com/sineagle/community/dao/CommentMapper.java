3e260880c40ad973b95167e7257c20531526d18d
sineagle <997991067@qq.com>
2022-11-17 18:32:59 +0800

    实现了添加评论功能

--- src/main/java/com/sineagle/community/dao/CommentMapper.java
+++ src/main/java/com/sineagle/community/dao/CommentMapper.java
@@ -10,4 +10,6 @@ public interface CommentMapper {
     List<Comment> selectCommentByEntity(int entityType, int entityId, int offset, int limit);
 
     int selectCountByEntity(int entityType, int entityId);
+
+    int insertComment(Comment comment);
 }
