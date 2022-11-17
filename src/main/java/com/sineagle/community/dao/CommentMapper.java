8d1a2e3b440cca519b491106119b199fdc208381
sineagle <997991067@qq.com>
2022-11-17 17:17:35 +0800

    实现了显示评论功能

---
+++ src/main/java/com/sineagle/community/dao/CommentMapper.java
@@ -0,0 +1,13 @@
+package com.sineagle.community.dao;
+
+import com.sineagle.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    List<Comment> selectCommentByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+}
