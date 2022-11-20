e99835b0a866e391ca565f695856b172e9c76912
sineagle <997991067@qq.com>
2022-11-20 19:19:30 +0800

    增加了统一处理异常

--- src/main/java/com/sineagle/community/controller/MessageController.java
+++ src/main/java/com/sineagle/community/controller/MessageController.java
@@ -30,6 +30,7 @@ public class MessageController {
     //私信列表
     @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
     public String getLetterLists(Model model, Page page) {
+        //Integer.valueOf("abc"); // 测试异常
         User user = hostHolder.getUser();
         // 设置分页信息
         page.setLimit(5);
@@ -115,6 +116,7 @@ public class MessageController {
     @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
     @ResponseBody
     public String sendLetter(String toName, String content) {
+        //Integer.valueOf("abc"); //测试异常
         User target = userService.findUserByName(toName);
         if (target == null) {
             return CommunityUtil.getJSONString(1, "目标用户不存在!");
