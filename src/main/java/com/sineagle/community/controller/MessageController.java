8f05eb2cb87432f7cfdfdd44252fd927617efe2f
sineagle <997991067@qq.com>
2022-11-20 10:19:39 +0800

    增加了删除私信功能

--- src/main/java/com/sineagle/community/controller/MessageController.java
+++ src/main/java/com/sineagle/community/controller/MessageController.java
@@ -136,6 +136,14 @@ public class MessageController {
 
         return CommunityUtil.getJSONString(0);
     }
+
+    @RequestMapping(path = "/letter/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String deleteLetter(int id) {
+        messageService.deleteMessage(id);
+        return CommunityUtil.getJSONString(0);
+    }
+
 }
 
 
