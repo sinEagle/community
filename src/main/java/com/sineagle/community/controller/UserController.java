2ca588b9c1e6d2a849afbb8a9d64190d39e22a76
sineagle <997991067@qq.com>
2022-11-23 16:36:35 +0800

    增加了关注、取消关注功能

--- src/main/java/com/sineagle/community/controller/UserController.java
+++ src/main/java/com/sineagle/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.sineagle.community.controller;
 
 import com.sineagle.community.annotation.LoginRequired;
 import com.sineagle.community.entity.User;
+import com.sineagle.community.service.FollowService;
 import com.sineagle.community.service.LikeService;
 import com.sineagle.community.service.UserService;
+import com.sineagle.community.util.CommunityConstant;
 import com.sineagle.community.util.CommunityUtil;
 import com.sineagle.community.util.HostHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -28,7 +30,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Value("${community.path.upload}")
@@ -48,6 +50,9 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -152,6 +157,20 @@ public class UserController {
         // 点赞数量
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount", likeCount);
+
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 是否已关注
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
+
         return "/site/profile";
     }
 
