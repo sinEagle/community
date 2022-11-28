31c42a2d718676b1e984d60dc7503e6246dfe46d
sineagle <997991067@qq.com>
2022-11-28 17:24:32 +0800

    整合Kafka

---
+++ src/test/java/com/sineagle/community/KafkaTests.java
@@ -0,0 +1,51 @@
+package com.sineagle.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() {
+        kafkaProducer.sendMessage("test", "你好！");
+        kafkaProducer.sendMessage("test", "在吗");
+        try {
+            Thread.sleep(1000*10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer {
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
+        System.out.println(record.value());
+        //logger.debug("{}", record.value());
+    }
+}
